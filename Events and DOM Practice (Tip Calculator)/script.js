c9eded6f3542e0572a8ef2ce8b41d47e89f18abd
ongon <ongon68@gmail.com>
2025-09-18 18:34:42 -0800

    finished the Tip Calculator project

--- Events and DOM Practice (Tip Calculator)/script.js
+++ Events and DOM Practice (Tip Calculator)/script.js
@@ -6,16 +6,28 @@ const calcTipBtn75 = document.getElementById("calcTipBtn7.5%");
 const calcTipBtn10 = document.getElementById("calcTipBtn10%");
 const tipAmount = document.getElementById("tipAmount");
 let tipTotal = "";
+const splitPeople = document.getElementById("splitPeople");
+const splitBill = document.getElementById("splitBill");
+const splitAmount = document.getElementById("splitAmount");
+let splitTotal = "";
 console.log(checkTotal);
 
 // Calculate tip for 5% of total check amount
 
 // Register button press
 calcTipBtn5.addEventListener("click", () => {
+    // Create a variable "bill" that parses checkTotal to see if it is a Float or Int
+    let bill = parseFloat(checkTotal.value);
+
+    // Throw error to user if they do not enter a integer or float value
+    if(isNaN(bill)) {
+        tipAmount.innerText = "Please enter a integer or float value"
+    }
     // Multiply check total by 5% and assign as tipTotal
-    tipTotal = checkTotal.value * 0.05;
-    console.log(tipTotal);
-    tipAmount.innerText = `Tip Total: $${tipTotal}.`
+    else {
+        tipTotal = bill * 0.05;
+        tipAmount.innerText = `Tip Total: $${tipTotal.toFixed(2)}.`
+    }
 });
 
 
@@ -23,9 +35,18 @@ calcTipBtn5.addEventListener("click", () => {
 
 // Register button press
 calcTipBtn75.addEventListener("click", () => {
+    // Create a variable "bill" that parses checkTotal to see if it is a Float or Int
+    let bill = parseFloat(checkTotal.value);
+
+    // Throw error to user if they do not enter a integer or float value
+    if(isNaN(bill)) {
+        tipAmount.innerText = "Please enter a integer or float value"
+    }
     // Multiply check total by 5% and assign as tipTotal
-    tipTotal = checkTotal.value * 0.075;
-    tipAmount.innerText = `Tip Total: $${tipTotal}.`
+    else {
+        tipTotal = bill * 0.075;
+        tipAmount.innerText = `Tip Total: $${tipTotal.toFixed(2)}.`
+    }
 });
 
 
@@ -33,7 +54,26 @@ calcTipBtn75.addEventListener("click", () => {
 
 // Register button press
 calcTipBtn10.addEventListener("click", () => {
+    // Create a variable "bill" that parses checkTotal to see if it is a Float or Int
+    let bill = parseFloat(checkTotal.value);
+
+    // Throw error to user if they do not enter a integer or float value
+    if(isNaN(bill)) {
+        tipAmount.innerText = "Please enter a integer or float value"
+    }
     // Multiply check total by 5% and assign as tipTotal
-    tipTotal = checkTotal.value * 0.1;
-    tipAmount.innerText = `Tip Total: $${tipTotal}.`
+    else {
+        tipTotal = bill * 0.1;
+        tipAmount.innerText = `Tip Total: $${tipTotal.toFixed(2)}.`
+    }
 });
+
+
+// Calculate split total
+splitBill.addEventListener("click", () => {
+    let bill = parseFloat(checkTotal.value);
+    let people = parseInt(splitPeople.value);
+    // Divide check total + tip amount by splitPeople
+    splitTotal = (bill + tipTotal) / people;
+    splitAmount.innerText = `Total bill after being split between ${people} people is ${splitTotal.toFixed(2)}.`
+})
